f0171a96c56fd7e7a02060fea242968822caa043
yurtaev <a.g.yurtaev@ya.ru>
2023-06-14 17:44:15 +0600

    Task13

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -9,9 +9,11 @@ Console.Write("Введите первое число: ");
 int num1 = int.Parse(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int num2 = int.Parse(Console.ReadLine());
-
-if (num1 % num2 == 0){
-    Console.WriteLine("Кратно");}
-else{
+if (num1 % num2 == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
     Console.WriteLine($"Не кратно, остаток - {num1 % num2}");
 }
