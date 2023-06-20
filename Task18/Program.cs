d2940a6491296a0b91eab1739dd1ee3f0b9197fe
yurtaev <a.g.yurtaev@ya.ru>
2023-06-20 12:29:51 +0600

    Task18 - 2

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -1,28 +1,64 @@
 ﻿// **Задача 18:** Напишите программу, которая по заданному номеру четверти,
 // показывает диапазон возможных координат точек в этой четверти (x и y)
 
+// Console.Clear();
+
+// Console.WriteLine("Введите номер четверти: ");
+// int num = int.Parse(Console.ReadLine());
+
+// if (num==1)
+// {
+//     Console.WriteLine("Диапазон возможных координат Х>0; Y>0");
+// }
+// else if (num==2)
+// {
+//     Console.WriteLine("Диапазон возможных координат Х<0; Y>0");
+// }
+// else if (num==3)
+// {
+//     Console.WriteLine("Диапазон возможных координат Х<0; Y<0");
+// }
+// else if (num==4)
+// {
+//     Console.WriteLine("Диапазон возможных координат Х>0; Y<0");
+// }
+// else
+// {
+//     Console.WriteLine("Введён неверный номер четверти");
+// }
+
+
+
 Console.Clear();
 
 Console.WriteLine("Введите номер четверти: ");
-int num = int.Parse(Console.ReadLine());
+int quarter = int.Parse(Console.ReadLine());
 
-if (num==1)
+switch(quarter)
 {
+    case 1:
+    {
     Console.WriteLine("Диапазон возможных координат Х>0; Y>0");
-}
-else if (num==2)
-{
+    break;
+    }
+    case 2:
+    {
     Console.WriteLine("Диапазон возможных координат Х<0; Y>0");
-}
-else if (num==3)
-{
+    break;
+    }
+    case 3:
+    {
     Console.WriteLine("Диапазон возможных координат Х<0; Y<0");
-}
-else if (num==4)
-{
+    break;
+    }
+    case 4:
+    {
     Console.WriteLine("Диапазон возможных координат Х>0; Y<0");
-}
-else
-{
+    break;
+    }
+    default:
+    {
     Console.WriteLine("Введён неверный номер четверти");
+    break;
+    }
 }
