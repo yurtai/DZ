0fa7c70051e58fddfc4f8602acd98f5f1b751838
yurtaev <a.g.yurtaev@ya.ru>
2023-06-21 16:18:58 +0600

    Task19

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -6,9 +6,9 @@
 
 Console.Clear();
 Console.Write("Введите первое число: ");
-int num1 = int.Parse(Console.ReadLine());
+int num1 = int.Parse(Console.ReadLine()!);
 Console.Write("Введите второе число: ");
-int num2 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine()!);
 if (num1 % num2 == 0)
 {
     Console.WriteLine("Кратно");
