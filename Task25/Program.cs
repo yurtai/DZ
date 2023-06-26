060e61fec90c6f5049fd867da214f345e7f76ad0
yurtaev <a.g.yurtaev@ya.ru>
2023-06-26 15:39:34 +0600

    Task29

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -3,18 +3,20 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-  Console.Write("Введите число A: ");
-  int numberA = Convert.ToInt32(Console.ReadLine());
-  Console.Write("Введите число B: ");
-  int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
 
-  int expon = Exp(numberA, numberB);
-  Console.WriteLine("Число A в степени B: " + expon);
+int expon = Exp(numberA, numberB);
+Console.WriteLine("Число A в степени B: " + expon);
 
-  int Exp(int numberA, int numberB){
-  int result = 1;
-  for(int i=1; i <= numberB; i++){
-    result = result * numberA;
-  }
+int Exp(int numberA, int numberB)
+{
+    int result = 1;
+    for (int i = 1; i <= numberB; i++)
+    {
+        result = result * numberA;
+    }
     return result;
 }
---
+++ Task29/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 29. Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+int[] array = new int[8];
+
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(100);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
